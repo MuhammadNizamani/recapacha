e2b9b7e4bdd340ca80cc505165d24547e7e10cf7
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-03-06 12:02:12 +0500

    add a file which has logic for setting label

---
+++ numberandcharacterlogic.py
@@ -0,0 +1,14 @@
+import string
+all_letter = string.ascii_lowercase + string.digits
+mapping = {}
+mapping_inv = {}
+i = 0
+for x in all_letter:
+    mapping[x] = i
+    mapping_inv[i] = x
+    i += 1
+
+
+num_chart = len(mapping)
+print(num_chart)
+print(mapping)
