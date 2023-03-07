9a1b152d0c1a23b154335396d68a65a2ee08e4f9
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-03-07 12:40:02 +0500

    adding comment in the dataLoederClass.py

--- dataLoaderClasslogic.py
+++ dataLoaderClasslogic.py
@@ -10,12 +10,20 @@ def remove_file_extension(filename):
 
 
 class CaptchaDataset(Dataset):
+    # this init function is a constructer which takes folder directory and a file (image) name
+    # using the self keyword making goble the varible name base_dir and image_filename
     def __init__(self, base_dir, image_filename):
         self.base_dir = base_dir
         self.image_filename = image_filename
 
+
+# The function len retrun the size of arry image or number of images in the dataset
+
     def __len__(self):
         return len(self.image_filename)
+
+
+# function getitem take index as a input and return the the name of image and the image
 
     def __getitem__(self, index):
         image_filename = self.image_filename[index]
@@ -24,7 +32,15 @@ class CaptchaDataset(Dataset):
         image = self.transfrom(image)
         label = remove_file_extension(image_filename)
         return (image, label)
+#this function take image as an imput and transfrom the image into tensor then normalize 
+# the image and retrun image as output 
 
+# Here is the explation of of function of transfroms
+# transformation normalizes the tensor image by subtracting the mean 
+# values specified in mean and dividing by the standard deviation 
+# values specified in std. The values in mean and std are expected to
+# be sequences of length 3, corresponding to the mean and standard 
+# deviation values for the red, green, and blue channels of the image.
     def transfrom(image):
         transform_op = transforms.Compose(
             [transforms.ToTensor(),
