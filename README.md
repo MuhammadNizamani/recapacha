a113400054529b4ef2b05a342780ac9b31601f36
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-03-07 12:45:23 +0500

    update the readme and main.py file

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# dectetion of CAPTCHA
+# Dectetion of CAPTCHA
+# If you see this need to improve please fix it or make it issue
 I am taking data from Kaggle try to find a way to recognize CAPACHA from images
 file name main.py content all right code and other file are for logic develpoment
--- main.py
+++ main.py
@@ -54,3 +54,43 @@ print(mapping_inv)
 def remove_file_extension(filename):
     """this function for removing extension like file.png it will convert it file"""
     return filename.split('.')[0]
+
+class CaptchaDataset(Dataset):
+    # this init function is a constructer which takes folder directory and a file (image) name
+    # using the self keyword making goble the varible name base_dir and image_filename
+    def __init__(self, base_dir, image_filename):
+        self.base_dir = base_dir
+        self.image_filename = image_filename
+
+
+# The function len retrun the size of arry image or number of images in the dataset
+
+    def __len__(self):
+        return len(self.image_filename)
+
+
+# function getitem take index as a input and return the the name of image and the image
+
+    def __getitem__(self, index):
+        image_filename = self.image_filename[index]
+        image_path = os.path.join(self.base_dir, image_filename)
+        image = Image.open(image_path).convert("RGB")
+        image = self.transfrom(image)
+        label = remove_file_extension(image_filename)
+        return (image, label)
+#this function take image as an imput and transfrom the image into tensor then normalize 
+# the image and retrun image as output 
+
+# Here is the explation of of function of transfroms
+# transformation normalizes the tensor image by subtracting the mean 
+# values specified in mean and dividing by the standard deviation 
+# values specified in std. The values in mean and std are expected to
+# be sequences of length 3, corresponding to the mean and standard 
+# deviation values for the red, green, and blue channels of the image.
+    def transfrom(image):
+        transform_op = transforms.Compose(
+            [transforms.ToTensor(),
+             transforms.Normalize(mean=(0.485, 0.456, 0.406),
+                                  std=(0.229, 0.224, 0.225))
+             ])
+        return transform_op(image)
