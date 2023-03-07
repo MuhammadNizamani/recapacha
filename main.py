3fb37f05d4e6560101c8b507c082a5f6c3ef1ab5
MuhammadNizamani <49721249+MuhammadNizamani@users.noreply.github.com>
2023-03-07 13:32:43 +0500

    there is issue in dataloeade

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from torch.utils.data import Dataset, DataLoader
 import torch
 import multiprocessing as mp
 
-# here in the begnining the i am deffing 
+# here in the begnining the i am deffing
 BATCH_SIZE = 16
 NUM_EPOCHS = 50
 LEARNING_RATE = 0.001
@@ -34,11 +34,12 @@ print(f'Total images: {len(images)}')
 print(f'Unique length of capthca: {unique_len}')
 
 
-#check for non-captcha if len of capthca not equal to 5
+# check for non-captcha if len of capthca not equal to 5
 for idx, filename in enumerate(images):
     if len(filename.split(".")[0]) != 5:
-        print(f'Found file "{filename}" at index {idx} that is non-captcha image')
-        images.remove(filename) 
+        print(
+            f'Found file "{filename}" at index {idx} that is non-captcha image')
+        images.remove(filename)
 
 print(f'After cleaning, there are: {len(images)} images')
 
@@ -46,7 +47,7 @@ train_images, test_images = train_test_split(images, random_state=0)
 print(f'Train images: {len(train_images)}')
 print(f'Test images: {len(test_images)}')
 
-# Character and numbers Mapping 
+# Character and numbers Mapping
 
 all_letter = string.ascii_lowercase + string.digits
 mapping = {}
@@ -59,16 +60,17 @@ for x in all_letter:
 
 
 num_chart = len(mapping)
-print(num_chart)
+# print(num_chart)
 # print(mapping)
-print(mapping_inv)
+# print(mapping_inv)
 
-#data loader now I am going to load images with 
+# data loader now I am going to load images with
 
 
 def remove_file_extension(filename):
     """this function for removing extension like file.png it will convert it file"""
     return filename.split('.')[0]
+
 
 class CaptchaDataset(Dataset):
     # this init function is a constructer which takes folder directory and a file (image) name
@@ -93,14 +95,14 @@ class CaptchaDataset(Dataset):
         image = self.transfrom(image)
         label = remove_file_extension(image_filename)
         return (image, label)
-#this function take image as an imput and transfrom the image into tensor then normalize 
-# the image and retrun image as output 
+# this function take image as an imput and transfrom the image into tensor then normalize
+# the image and retrun image as output
 
 # Here is the explation of of function of transfroms
-# transformation normalizes the tensor image by subtracting the mean 
-# values specified in mean and dividing by the standard deviation 
+# transformation normalizes the tensor image by subtracting the mean
+# values specified in mean and dividing by the standard deviation
 # values specified in std. The values in mean and std are expected to
-# be sequences of length 3, corresponding to the mean and standard 
+# be sequences of length 3, corresponding to the mean and standard
 # deviation values for the red, green, and blue channels of the image.
     def transfrom(image):
         transform_op = transforms.Compose(
@@ -109,3 +111,27 @@ class CaptchaDataset(Dataset):
                                   std=(0.229, 0.224, 0.225))
              ])
         return transform_op(image)
+
+
+# loading data in  as train and test dataset
+train_dataset = CaptchaDataset(path, train_images)
+test_dataset = CaptchaDataset(path, test_images)
+
+train_loader = DataLoader(
+    train_dataset, batch_size=BATCH_SIZE, num_workers=cpu_count, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE,
+                         num_workers=cpu_count, shuffle=False)
+
+print(f'{len(train_loader)} batches in the train_loader')
+print(f'{len(test_loader)} batches in the test_loader')
+
+
+# below code is not working for some expetion and I am trying to solve
+try:
+    batch_images, batch_labels = iter(train_loader).next()
+except:
+    print("An exception occurred")
+
+
+print(batch_images.shape)
+print(batch_labels)
